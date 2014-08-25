9d9d88eaf9ff31e0869ac8250eeeb774088886d6
Adam Legg <adam.legg@gmail.com>
2014-08-25 15:35:39 -0600

    flash functional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
   $('body').append('<h1>Todoly</h1>');
 
   $('body').append("<form><input id='todo' type='text'> \
-      <input type='submit'></form></div>");
+      <input type='submit' class='submit' value='Create Todo'></form></div>");
 
-
+  $('body').append('<p class="flash"></p>');
 
 
 
@@ -20,5 +20,47 @@ $(document).ready(function() {
      $('body').append('<li>' + toDo + '</li>')
 
   });
+
+
+  (function($) {
+    $.fn.flash_message = function(options) {
+
+      options = $.extend({
+        text: 'Todo Created',
+        time: 5000,
+        how: 'before',
+        class_name: ''
+      }, options);
+
+      return $(this).each(function() {
+        if( $(this).parent().find('.flash_message').get(0) )
+          return;
+
+        var message = $('<span />', {
+          'class': 'flash_message ' + options.class_name,
+          text: options.text
+        }).hide().fadeIn('fast');
+
+        $(this)[options.how](message);
+
+        message.delay(options.time).fadeOut('normal', function() {
+          $(this).remove();
+        });
+
+      });
+    };
+  })(jQuery);
+
+
+
+  $('.submit').click(function() {
+    $('body p').flash_message({
+      text: "Todo created",
+      how: 'append',
+      time: 5000,
+      color: 'green'
+
+    })
+  })
 
 });

904033e4640809f0a090927ffff19ba56bb86d6b
Adam Legg <adam.legg@gmail.com>
2014-08-25 13:54:56 -0600

    homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 
 $(document).ready(function() {
-  $('body').append("<h1>hello world, I'm from only javascript!</h1>");
+  $('body').append('<h1>Todoly</h1>');
+
+  $('h1').append("<form><input id='todo' type='text'> \
+      <input type='submit'></form></div>");
+
+
+
+  var todo = document.getElementById("todo").value;
+  $('body').append("<li></li>");
+  console.log('todo')
+
 });
